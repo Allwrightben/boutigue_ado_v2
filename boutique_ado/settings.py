2101f87228f32035da8a8cea2625ea8a65c9725e
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-01 19:50:50 +0100

    Add product/forms.py file

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,3 +181,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+# Default from email
+DEFAULT_FROM_EMAIL = 'benallwright11@gmail.com'
