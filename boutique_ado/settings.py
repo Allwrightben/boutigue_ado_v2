52beb89d92caa752c3f00de6c4629940553e4364
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-03 21:01:33 +0100

    Add config vars to hero and update settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l$7kiul)5u)8pec^$t%2nh9y_5)f^iam4@swrvjv%-kiwl334*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'bens-boutique-ado.herokuapp.com',
