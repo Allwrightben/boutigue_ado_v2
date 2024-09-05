f852819b44367298579d93007ff59eec73a5faad
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-05 15:55:17 +0100

    Fix local server not accepting my .env file by adding one manually in the terminal

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,14 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
-    'https://bens-boutique-ado-cfc020991ecb.herokuapp.com'
+    'https://bens-boutique-ado-cfc020991ecb.herokuapp.com',
     'bens-boutique-ado.herokuapp.com',
     'localhost',
     'http://127.0.0.1:8000/',
